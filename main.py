aca58dd361e08ba97dad08e9c1b13a52acd52775
Kagiso Mlambo <missk.charmaine@gmail.com>
2026-08-16 02:01:44 +0200

    Pushing main

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from database.databases import *
 
-def register():
-    name = input("Name: ")
-    surname = input("Surname: ")
-    email = input("Email: ")
+def register(request):
+    # name = input("Name: ")
+    # surname = input("Surname: ")
+    # email = input("Email: ")
 
-    identity = input("Are you using a Passport or South African ID (type 'P' for Passport or 'I' for ID): ").lower()
-    if identity == "i":
-        identity = input("Input your Id number: ")
-    else:
-        identity = input("Input your Passport number: ")
+    # identity = input("Are you using a Passport or South African ID (type 'P' for Passport or 'I' for ID): ").lower()
+    # if identity == "i":
+    #     identity = input("Input your Id number: ")
+    # else:
+    #     identity = input("Input your Passport number: ")
 
-    address = input("Address: ")
-    password = input("Enter a password: ")
+    # address = input("Address: ")
+    # password = input("Enter a password: ")
+    name = request["name"]
+    surname = request["surname"]
+    email = request["email"]
+    identity = request["identity"]
+    address = request["address"]
+    password = request["password"]
 
     return create_user(identity, name, surname, email, password, address)
 
-def log_in():
-    email = input("Enter your email: ")
-    password = input("Enter your password: ")
+def log_in(request):
+    # email = input("Enter your email: ")
+    # password = input("Enter your password: ")
+    email = request["email"]
+    password = request["password"]
     return log_in(email, password)
 
 def main():
